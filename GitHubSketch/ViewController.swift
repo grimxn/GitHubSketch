c0547690cb36810b0bac0475744c11d05f614fd5
Alan Faichney <afaichney@mac.com>
2018-01-31 16:42:53 +0000

    Initial Commit

---
+++ GitHubSketch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitHubSketch
+//
+//  Created by Alan Faichney on 31/01/2018.
+//  Copyright © 2018 Alan Faichney. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
