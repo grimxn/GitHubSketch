9b1f0ff4c27c834027c1cd5ce1327899ef77faf4
@grimxn <grimxn@grimxn.com>
2018-01-31 17:16:05 +0000

    Commit 2

---
+++ GitHubSketch/WellDodge.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  GitHubSketch
+//
+//  Created by Alan Faichney on 31/01/2018.
+//  Copyright © 2018 Alan Faichney. All rights reserved.
+//
+
+import Foundation
+
+let mipi = 180.toRadians
